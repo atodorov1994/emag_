8b23073245cfecdf4218167a3fa51ba69765458d
Aleksandar Todorov <atodorov1994@gmail.com>
2022-02-22 12:53:30 +0200

    Email service

--- src/main/java/com/emag/service/EmailService.java
+++ src/main/java/com/emag/service/EmailService.java
@@ -1,33 +1,5 @@
 package com.emag.service;
 
-<<<<<<< HEAD
-import com.emag.exception.EmailNotSendException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Component
-public class EmailService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ittalents.emag@gmail.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        try {
-            mailSender.send(message);
-        }catch (Exception e){
-            e.printStackTrace();
-            throw new EmailNotSendException("Email failed to send") ;
-        }
-=======
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -74,6 +46,5 @@ public class EmailService {
         props.put("mail.debug", "true");
 
         return mailSender;
->>>>>>> refs/remotes/origin/main
     }
 }
