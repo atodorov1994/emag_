8ff549011e47ed833c2b75eb1797d02680ff9398
Victor Mladenov <victor.m99@icloud.com>
2022-02-23 20:45:22 +0200

    Presentation add

--- src/main/java/com/emag/EmagApplication.java
+++ src/main/java/com/emag/EmagApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@Configuration
 @EnableScheduling
 public class EmagApplication {
 
--- src/main/java/com/emag/service/Scheduler.java
+++ src/main/java/com/emag/service/Scheduler.java
@@ -2,22 +2,29 @@ package com.emag.service;
 
 import com.emag.model.pojo.Discount;
 import com.emag.model.pojo.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+@Configuration
+@EnableScheduling
 public class Scheduler extends AbstractService{
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(cron = "* 44 16 * * *")
     void checkExpiredDiscounts(){
+        System.out.println("Discount deletion started");
         List<Discount> discounts = discountRepository.findAll();
         discounts.forEach(d -> {
             if (d.getExpireDate().toLocalDateTime().isBefore(LocalDateTime.now())){
                 List<Product> products = productRepository.findAllByDiscount(d);
                 products.forEach(p -> {
-                    double price = p.getPrice();
-                    double regularPrice = price*(d.getDiscountPercent()/100+1);
+                    p.setDiscountedPrice(null);
+                    p.setDiscount(null);
+                    productRepository.save(p);
                 } );
                 discountRepository.delete(d);
             }
